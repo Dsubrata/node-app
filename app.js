2af8514e9e3099028eeab4ac3e55172b6b7711d9
Subrata Das <Dsubrata@users.noreply.github.com>
2025-08-15 12:25:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
     const http = require('http');
 
     const hostname = '127.0.0.1'; // localhost
-    const port = 3000;
+    const port = 8080;
 
     const server = http.createServer((req, res) => {
       res.statusCode = 200;
@@ -11,4 +11,5 @@
 
     server.listen(port, hostname, () => {
       console.log(`Server running at http://${hostname}:${port}/`);
+
     });
